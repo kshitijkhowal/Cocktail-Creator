360710d8a6352abf30832ab6571a8ebd75652440
kshitijkhowal <kshitij.durgesh@gmail.com>
2024-07-27 05:10:45 +0530

    create home page

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ import axios from "axios";
 const app=express();
 const port=3000;
 
+app.use(express.static("public"));
 
 app.listen(port,()=>{
     console.log(`Server is listening on port ${port}`);
+})
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
 })
