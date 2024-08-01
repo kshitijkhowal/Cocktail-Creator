5b6eed055f04f81ac689d61951f4c4c73f5a099b
kshitijkhowal <kshitij.durgesh@gmail.com>
2024-08-01 20:16:12 +0530

    safe point trying to add recipe page

--- index.js
+++ index.js
@@ -2,55 +2,55 @@ import express from "express";
 import bodyParser from "body-parser"
 import axios from "axios";
 
-const app=express();
-const port=3000;
-const API_URL="https://www.thecocktaildb.com/api/json/v1/1";
+const app = express();
+const port = 3000;
+const API_URL = "https://www.thecocktaildb.com/api/json/v1/1";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 })
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("index.ejs");
 })
-app.get("/search",(req,res)=>{
+app.get("/search", (req, res) => {
     res.render("search.ejs");
 })
-app.post("/search",async(req,res)=>{
-    const drinkName=req.body["cocktail_name"];
+app.post("/search", async (req, res) => {
+    const drinkName = req.body["cocktail_name"];
     // console.log(drinkName);
     try {
         const response = await axios.get(`${API_URL}/search.php?s=${drinkName}`);
         const result = response.data;
         // console.log(result);
         res.render("search.ejs", { recipies: result });
-      } catch (error) {
+    } catch (error) {
         console.error("Failed to make request:", error.message);
         res.render("index.ejs", {
-          error: error.message,
+            error: error.message,
         });
-      }
- 
+    }
+
 })
 
-app.post("/recipie",async(req,res)=>{
-    const userPickedDrink=req.body["drink_ID"];
+app.post("/recipie", async (req, res) => {
+    const userPickedDrink = req.body["drink_ID"];
     console.log(userPickedDrink);
 
     try {
         const response = await axios.get(`${API_URL}/lookup.php?i=${userPickedDrink}`);
         const result = response.data;
         console.log(result);
-        res.render("recipie.ejs");
-      } catch (error) {
+        res.render("recipie.ejs", { chosenDrink : result });
+    } catch (error) {
         console.error("Failed to make request:", error.message);
         res.render("index.ejs", {
-          error: error.message,
+            error: error.message,
         });
-      }
+    }
 })
 
 
