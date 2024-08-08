83a8b6a38d1b3d6d7545cd374e97d20a548f7c77
kshitijkhowal <kshitij.durgesh@gmail.com>
2024-08-08 21:49:46 +0530

    recipe page working

--- index.js
+++ index.js
@@ -36,15 +36,28 @@ app.post("/search", async (req, res) => {
 
 })
 
+
 app.post("/recipie", async (req, res) => {
     const userPickedDrink = req.body["drink_ID"];
     console.log(userPickedDrink);
-
+    
     try {
         const response = await axios.get(`${API_URL}/lookup.php?i=${userPickedDrink}`);
         const result = response.data;
-        console.log(result);
-        res.render("recipie.ejs", { chosenDrink : result });
+        const temp=result.drinks[0];
+        console.log(temp);
+        let ingredients=[];
+        for(var i=1;i<=15;i++){
+            if(temp["strIngredient"+i]===null) break;
+            else{
+                const element=temp["strMeasure"+i]+" "+temp["strIngredient"+i]
+                ingredients.push(element);
+            }
+        }
+        res.render("recipie.ejs", { 
+            chosenDrink : result,
+            ingredients: ingredients,
+         });
     } catch (error) {
         console.error("Failed to make request:", error.message);
         res.render("index.ejs", {
